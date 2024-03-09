3e89088ca912e5a0206bf71c0a6a007e4000f855
cozmo1013 <160294993+cozmo1013@users.noreply.github.com>
2024-03-08 18:52:38 -0600

    Create Gen.cpp
    Password Generator

---
+++ Gen.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <string>
+using namespace std;
+
+int main() {
+    string pswrd = "";
+    int val;
+    char comp[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+                   'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
+                   'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
+                   'u', 'v', 'w', 'x', 'y', 'z', '|', '/', '\\', '+',
+                   ')', '(', '-', '*', '&', '^', '%', '$', '#', '@',
+                   '!', '~', '`', '=', '<', '>', ';', ':', '.', ',',
+                   'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
+                   'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T',
+                   'U', 'V', 'W', 'X', 'Y', 'Z'};
+
+    srand(time(0));
+    for(int i = 0; i < 72; i++) {
+        val = rand() % 87;
+        pswrd += comp[val];
+    }
+
+    cout << pswrd << endl;
+
+    system("pause");
+    return 0;
+}
